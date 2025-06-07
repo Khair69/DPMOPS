5f1e2194fcdbe685df796e6dab1d9183cfccd0ea
Khair <khair04@outlook.com>
2025-06-07 19:08:04 +0300

    added datatables.js to all the tables

---
+++ DPMOPS/wwwroot/js/tableStyling.js
@@ -0,0 +1,83 @@
+﻿$(document).ready(function () {
+    $('#org-table').DataTable({
+        lengthChange: false,
+        language: {
+            search: "بحث"
+        },
+        info: false,
+        order: [[1, "asc"]]
+    });
+});
+
+$(document).ready(function () {
+    $('#account-table').DataTable({
+        lengthChange: false,
+        language: {
+            search: "بحث"
+        },
+        info: false
+    });
+});
+
+$(document).ready(function () {
+    $('#admin-table').DataTable({
+        lengthChange: false,
+        language: {
+            search: "بحث"
+        },
+        info: false
+    });
+});
+
+$(document).ready(function () {
+    $('#request-table').DataTable({
+        lengthChange: false,
+        language: {
+            search: "بحث"
+        },
+        info: false,
+        order: [[7,"desc"]]
+    });
+});
+
+$(document).ready(function () {
+    $('#city-table').DataTable({
+        lengthChange: false,
+        language: {
+            search: "بحث"
+        },
+        info: false,
+        order: [[0, "asc"]]
+    });
+});
+
+$(document).ready(function () {
+    $('#district-table').DataTable({
+        lengthChange: false,
+        language: {
+            search: "بحث"
+        },
+        info: false,
+        order: [[0, "asc"]]
+    });
+});
+
+$(document).ready(function () {
+    $('#employee-table').DataTable({
+        lengthChange: false,
+        language: {
+            search: "بحث"
+        },
+        info: false
+    });
+});
+
+$(document).ready(function () {
+    $('#orgAdmin-table').DataTable({
+        lengthChange: false,
+        language: {
+            search: "بحث"
+        },
+        info: false
+    });
+});
