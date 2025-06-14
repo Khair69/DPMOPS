1e7aa359917d33c6d56679d70095e0d3e745e65a
Khair <khair04@outlook.com>
2025-06-14 18:20:00 +0300

    added NOT COMPLETE comment section modal to the info page

--- DPMOPS/Services/Comment/Dtos/AddCommentDto.cs
+++ DPMOPS/Services/Comment/Dtos/AddCommentDto.cs
@@ -10,7 +10,7 @@ namespace DPMOPS.Services.Comment.Dtos
         public Guid ServiceRequestId { get; set; }
 
         [Required(ErrorMessage = "{0} مطلوب")]
-        [Display(Name = "المحتوى")]
+        [Display(Name = "التعليق")]
         [StringLength(100, ErrorMessage = "{0} يجب ان يكون اقصر من {1} حرف و اطول من {2} حرف ", MinimumLength = 2)]
         public string? Content { get; set; }
     }
