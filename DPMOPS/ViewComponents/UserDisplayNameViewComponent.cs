2defb4466ea1b4fa4886ee1a2b8e551584ee3253
Khair <khair04@outlook.com>
2025-06-15 23:02:54 +0300

    SIDEBAR BABY

---
+++ DPMOPS/ViewComponents/UserDisplayNameViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using DPMOPS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DPMOPS.ViewComponents
+{
+    public class UserDisplayNameViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserDisplayNameViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var name = user?.FirstName + " " + user?.LastName ?? "Guest";
+            return View("Default", name);
+        }
+    }
+
+}
