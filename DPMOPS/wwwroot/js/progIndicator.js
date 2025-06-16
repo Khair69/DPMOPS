960855c49fd3987476f83d09f253564038304443
Khair <khair04@outlook.com>
2025-06-16 18:55:40 +0300

    better progress indicator

--- DPMOPS/wwwroot/js/progIndicator.js
+++ DPMOPS/wwwroot/js/progIndicator.js
@@ -11,6 +11,13 @@
     const thirdDot = document.querySelector('.third-dot');
     const fourthDot = document.querySelector('.fourth-dot');
 
+    const firstDotLabel = document.querySelector('.first-dot-label');
+    const secondDotLabel = document.querySelector('.second-dot-label');
+    const thirdDotLabel = document.querySelector('.third-dot-label');
+    const fourthDotLabel = document.querySelector('.fourth-dot-label');
+
+    const middleText = document.querySelector('.middle-text');
+
     switch (widthValue) {
         case 1: {
             progressBar.style.width = '0%';
@@ -49,20 +56,46 @@
             progressBar.style.width = '100%';
             progressBar.classList.add('bg-warning');
 
-            if (firstDot) firstDot.style.backgroundColor = 'var(--warning-dot-color)';
-            if (secondDot) secondDot.style.backgroundColor = 'var(--warning-dot-color)';
-            if (thirdDot) thirdDot.style.backgroundColor = 'var(--warning-dot-color)';
-            if (fourthDot) fourthDot.style.backgroundColor = 'var(--warning-dot-color)';
+            if (firstDot) firstDot.classList.add('hide');
+            if (secondDot) secondDot.classList.add('hide');
+            if (thirdDot) thirdDot.classList.add('hide');
+            if (fourthDot) fourthDot.classList.add('hide');
+
+            if (firstDotLabel) firstDotLabel.classList.add('hide');
+            if (secondDotLabel) secondDotLabel.classList.add('hide');
+            if (thirdDotLabel) thirdDotLabel.classList.add('hide');
+            if (fourthDotLabel) fourthDotLabel.classList.add('hide');
+
+            if (middleText) {
+                middleText.classList.remove('hide');
+                middleText.innerHTML = '<i class="bi bi-exclamation-triangle-fill"></i>';
+                middleText.append(' معلقة');
+                middleText.classList.add('text-bg-warning');
+            }
+
             break;
         }
         case 5: {
             progressBar.style.width = '100%';
             progressBar.classList.add('bg-danger');
 
-            if (firstDot) firstDot.style.backgroundColor = 'var(--danger-dot-color)';
-            if (secondDot) secondDot.style.backgroundColor = 'var(--danger-dot-color)';
-            if (thirdDot) thirdDot.style.backgroundColor = 'var(--danger-dot-color)';
-            if (fourthDot) fourthDot.style.backgroundColor = 'var(--danger-dot-color)';
+            if (firstDot) firstDot.classList.add('hide');
+            if (secondDot) secondDot.classList.add('hide');
+            if (thirdDot) thirdDot.classList.add('hide');
+            if (fourthDot) fourthDot.classList.add('hide');
+
+            if (firstDotLabel) firstDotLabel.classList.add('hide');
+            if (secondDotLabel) secondDotLabel.classList.add('hide');
+            if (thirdDotLabel) thirdDotLabel.classList.add('hide');
+            if (fourthDotLabel) fourthDotLabel.classList.add('hide');
+
+            if (middleText) {
+                middleText.classList.remove('hide');
+                middleText.innerHTML = '<i class="bi bi-x-octagon-fill"></i>';
+                middleText.append(' مرفوضة');
+                middleText.classList.add('text-bg-danger');
+            }
+
             break;
         }
         case 6: {
