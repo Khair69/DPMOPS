da1669939355265ada8b1de334d2122bc0d02f27
Khair <khair04@outlook.com>
2025-06-24 20:44:56 +0300

    light mode

--- DPMOPS/Pages/Calendar/Index.cshtml.cs
+++ DPMOPS/Pages/Calendar/Index.cshtml.cs
@@ -1,9 +1,12 @@
 #nullable disable
+using DPMOPS.Enums;
 using DPMOPS.Services.Appointment;
 using DPMOPS.Services.Appointment.Dtos;
+using DPMOPS.Services.UserClaim;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 using System.Security.Claims;
 
 namespace DPMOPS.Pages.Calendar
@@ -12,36 +15,45 @@ namespace DPMOPS.Pages.Calendar
     public class IndexModel : PageModel
     {
         private readonly IAppointmentService _appointmentService;
+        private readonly IUserClaimService _userClaimService;
 
-        public IndexModel(IAppointmentService appointmentService)
+        public IndexModel(IAppointmentService appointmentService,
+            IUserClaimService userClaimService)
         {
             _appointmentService = appointmentService;
+            _userClaimService = userClaimService;
         }
 
         public IActionResult OnGet()
         {
-            if (User.HasClaim("IsAdmin", "true"))
+            var userType = _userClaimService.ResolveUserType(User);
+            if (userType is UserType.Citizen or UserType.Employee or UserType.OrgAdmin)
             {
-                return new ForbidResult();
+                return Page();
             }
-            return Page();
+            return Forbid();
         }
 
         public async Task<JsonResult> OnGetGetAppointmentsAsync()
         {
             IList<AppointmentDto> appointments = new List<AppointmentDto>();
-            if (User.HasClaim("IsOrgAdmin", "true"))
+            var userType = _userClaimService.ResolveUserType(User);
+            if (userType == UserType.OrgAdmin)
             {
                 var orgId = User.FindFirst("OrganizationId")?.Value;
                 appointments = await _appointmentService.GetOrganizationAppointmentsAsync(Guid.Parse(orgId));
             }
-            else if (User.FindFirst("OrganizationId")?.Value != null)
+            else if (userType == UserType.Employee)
             {
                 appointments = await _appointmentService.GetEmployeeAppointmentsAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
             }
-            else
+            else if (userType == UserType.Citizen)
             {
                 appointments = await _appointmentService.GetCitizenAppointmentsAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            }
+            else
+            {
+                return new JsonResult("{}");
             }
 
             var jsonAp = appointments.Select(a => new
--- DPMOPS/wwwroot/js/site.js
+++ DPMOPS/wwwroot/js/site.js
@@ -1,1 +1,31 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const checkbox = document.getElementById("themeToggleCheckbox");
+    const html = document.documentElement;
+    const aside = document.getElementById("Aside");
+
+    function applyTheme(theme) {
+        html.setAttribute("data-bs-theme", theme);
+        aside.setAttribute("data-bs-theme", theme);
+
+        if (theme === "dark") {
+            aside.classList.remove("bg-light");
+            aside.classList.add("bg-dark");
+            document.body.classList.add("dark");
+        } else {
+            aside.classList.remove("bg-dark");
+            aside.classList.add("bg-light");
+            document.body.classList.remove("dark");
+        }
+
+        localStorage.setItem("theme", theme);
+    }
+
+    const savedTheme = localStorage.getItem("theme") || "light";
+    checkbox.checked = savedTheme === "light";
+    applyTheme(savedTheme);
+
+    checkbox.addEventListener("change", function () {
+        const theme = this.checked ? "light" : "dark";
+        applyTheme(theme);
+    });
+});
